6b169712f8e72369530637700e78eee11cd62d67
priyanka381 <84623060+priyanka381@users.noreply.github.com>
2021-11-25 17:30:55 +0530

    redux app

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Priya", age: 21, email: "priyankavgs@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
